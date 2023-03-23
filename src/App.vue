a01e39cbc0c4c29e0709f192e26778d582f541e7
崔东阳 <duidongyang@douchexiaoer.com>
2023-03-23 16:56:29 +0800

    feat: ref的应用

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>{{man}}</div>
+  <hr>
+  <div>shallowRef:{{ man2 }}</div>
+  <hr>
+  <div>customRef: {{ man3 }}</div>
+  <button @click="change">change</button>
 </template>
 
+<script setup lang='ts'>
+import { ref, Ref, isRef, shallowRef, triggerRef, customRef } from 'vue'
+
+function MyRef<T>(value: T) {
+  return customRef((track, trigger) => {
+    let timer: any
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(nval) {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          console.log(1);
+          value = nval
+          timer = null;
+          trigger()
+        }, 500)
+      }
+    }
+  })
+}
+const man3 = MyRef<string>('customRef小曼')
+type M = {
+  name: string
+}
+// let man = ref<M>({name: '小曼'})
+// ref: 深层响应, shallowRef: 浅层响应
+// ref 和 shallowRef 不能一块儿写.会影响shallowRef视图更新
+let man:Ref<M> = ref({name: '小曼'})
+  let man2:Ref<M> = shallowRef({name: '小曼2'})
+const change = () => {
+  // man.value.name = '我是ref'
+  // man2.value.name = '我被影响了'
+  // triggerRef(man2)
+  // man2.value = {
+  //   name: '大曼2'
+  // }
+  // console.log(isRef(man));
+  man3.value = '我变了'
+  console.log(man3);
+  
+}
+</script>
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
